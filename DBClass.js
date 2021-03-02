d174482b1f79ebd46784b65efab9b47ff72b39e7
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-02 19:11:51 +0200

    added data base class

---
+++ DBClass.js
@@ -0,0 +1,91 @@
+const fs = require("fs");
+
+class DataBase {
+  constructor() {
+    fs.readFile("./data/urlJson.json", (err, data) => {
+      if (err) {
+        throw new Error(err);
+      } else {
+        this.urlObject = JSON.parse(data);
+      }
+    });
+  }
+  createNewUrl(originalUrl) {
+    const url = {};
+    url.date = createDate();
+    url.originalUrl = originalUrl;
+    url.shortUrlId = createRandomSequence();
+    url.redirects = 0;
+    this.urlObject.urlArray.push(url);
+    fs.writeFile(
+      "./data/urlJson.json",
+      JSON.stringify(this.urlObject, null, 4),
+      (err) => {
+        if (err) {
+          throw new Error(err);
+        }
+      }
+    );
+  }
+  getUrls() {
+    return this.urlObject;
+  }
+}
+
+function createDate() {
+  const date = new Date();
+  let newDate = date.toISOString().split(".")[0];
+  return newDate.split("T")[0] + " " + newDate.split("T")[1];
+}
+
+const charters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  0,
+];
+const numbers = [];
+function createRandomSequence() {
+  let randomString = "";
+  for (let i = 0; i < 5; i++) {
+    const char = charters[Math.floor(Math.random() * charters.length)];
+    randomString += char;
+  }
+  return randomString;
+}
+
+const dataBase = new DataBase();
+
+module.exports = dataBase;
