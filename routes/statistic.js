20d27a7e203330a21577d3b1a5c9f60f8dbf967d
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-02 19:19:11 +0200

    fix statistic

--- routes/statistic.js
+++ routes/statistic.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const dataBase = require("../DBClass");
 const axios = require("axios");
 
-router.get("/:shorturl-id", async (req, res) => {
-  const id = req.params["shorturl-id"];
+router.get("/:shorturl_id", async (req, res) => {
+  const id = req.params["shorturl_id"];
   const { urlArray } = dataBase.getUrls();
   const url = urlArray.filter((value) => value.shortUrlId === id);
   res.json(url);
