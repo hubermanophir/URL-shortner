d95b6c2522662493cf84fb933cdd14f4bfa1708e
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-03 12:56:24 +0200

    quick folder cleanup

--- routes/shorturl.js
+++ routes/shorturl.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const dataBase = require("../DBClass");
-const array = require("../views/script");
 
 router.get("/", (req, res) => {
   res.json(dataBase.getUrls());
