b4fffe0ad6b2ea783d7fc1f632855c9ae96d1548
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-05 18:43:14 +0200

    a bit of html

--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
 const submitBtn = document.getElementById("submit");
 const userUrl = document.getElementById("url_input");
+const shortUrlContainer = document.getElementById("short-url-container");
+const allShortLinks = document.getElementById("all-short-link");
 submitBtn.addEventListener("click", async (e) => {
   e.preventDefault();
   const value = userUrl.value;
+  if (shortUrlContainer.childNodes.length !== 0) {
+    shortUrlContainer.lastChild.remove();
+  }
   await postingNewUrl(value);
 });
 
@@ -15,7 +20,21 @@ async function postingNewUrl(url) {
         url: url,
       },
     });
+    console.log(response.data.shortUrlId);
+    makeShortUrlDiv(response.data.shortUrlId);
   } catch (err) {
     throw err.response.data.error;
   }
+}
+
+function makeShortUrlDiv(shortUrl) {
+  const div = document.createElement("div");
+  div.setAttribute("id", "short-url");
+  const a = document.createElement("a");
+  const shortAddress = `http://localhost:3000/api/shorturl/${shortUrl}`;
+  a.setAttribute("href", shortAddress);
+  a.setAttribute("target", "_blank");
+  a.innerText = shortAddress;
+  div.appendChild(a);
+  shortUrlContainer.appendChild(div);
 }
