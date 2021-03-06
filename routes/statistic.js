994465ce9f7636a79551eb9dc424ddec82e88c02
hubermanophir <77234895+hubermanophir@users.noreply.github.com>
2021-03-06 22:30:22 +0200

    comments if statistics route

--- routes/statistic.js
+++ routes/statistic.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const dataBase = require("../DBClass");
 const axios = require("axios");
 
+//gets the statistics of a specific short url
 router.get("/:shorturl_id", async (req, res) => {
   const id = req.params["shorturl_id"];
   const { urlArray } = dataBase.getUrls();
